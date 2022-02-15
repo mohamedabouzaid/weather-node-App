331a91bab8fc59cf09b4a19e8dbaaf031894d44e
abouzaid <mohmmdabouzaid@gmail.com>
2022-02-15 15:59:35 +0200

    add search interface with featch method

--- src/utils/geocoging.js
+++ src/utils/geocoging.js
@@ -5,16 +5,16 @@ const geocoding = (address,callback) => {
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
       encodeURI( address)    +
       ".json?access_token=pk.eyJ1IjoiYWJvdXphaWQxIiwiYSI6ImNremxueDRuaTE3YmcybmxsZnVzNnR0b2EifQ.imSdT_lql9jCx6oZQ9FDUQ&limit=1";
-     
-    request({ url, json: true }, (error, res) => {
+   
+    request({ url, json: true }, (error, {body}={}) => {
       if (error) {
         callback("Unable to connect to location services!");
-      } else if (res.body.features.length === 0) {
+      } else if (body.features.length === 0) {
         callback("Unable to find location. Try another search.");
       } else {
-        const latitude = res.body.features[0].center[1];
-        const longitude = res.body.features[0].center[0];
-        const place=res.body.features[0].place_name;
+        const latitude = body.features[0].center[1];
+        const longitude = body.features[0].center[0];
+        const place=body.features[0].place_name;
         callback(undefined,{latitude, longitude,place});
       }
     });
