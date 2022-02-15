b60b8095e87a5849b2fb912ed7b507d7bd4fb786
abouzaid <mohmmdabouzaid@gmail.com>
2022-02-15 16:56:50 +0200

    add ignore file

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+
+const weatherForm = document.querySelector('form')
+const search=document.querySelector('input')
+const paragraphLocation=document.getElementById('location')
+const paragraphForecast=document.getElementById('forecast')
+  weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    console.log(location);
+   if(location){
+    fetch('http://localhost:3000/weather?address='+location).then((res)=>{
+        res.json().then((data)=>{
+           
+          if (data.error) {
+              paragraph.innerHTML=data.error
+              } else {
+                   
+                  paragraphLocation.innerHTML=data.place
+                  paragraphForecast.textContent=data.forecast
+             
+              }
+        })
+      })
+   }
+
+
+})
